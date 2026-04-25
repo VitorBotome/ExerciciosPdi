01995d9c43c2c8aa9af7e6dfc030f297f7938db1
Vitor Botomé <vitorbotome@gmail.com>
2026-04-25 14:36:37 -0300

    add while

--- ExerciciosPdi/Program.cs
+++ ExerciciosPdi/Program.cs
@@ -6,8 +6,75 @@ class Program
 {
     static void Main()
     {
-        
+        bool sair = true;
+
+        while (sair)
+        {
+            exibirMenu();
+
+            int opcao = int.Parse(Console.ReadLine()!);
+
+            MenuOpcao escolha = (MenuOpcao)opcao;
+
+            switch (escolha)
+            {
+                case MenuOpcao.Sair:
+                    sair = false;
+                    Console.WriteLine("Voce saiu do programa.");
+                    break;
+                case MenuOpcao.NomeUsuario:
+                    //Crie um programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas personalizada com o nome dele é exibida:  Olá, Welisson! Seja muito bem-vindo!
+                    Exercicio1.NomeUsuario();
+                    break;
+                case MenuOpcao.NomeCompleto:
+                    //Crie um programa que concatene um nome e um sobrenome inseridos pelo usuário e ao final exiba o nome completo.
+                    Exercicio2.NomeSobrenome();
+                    break;
+                case MenuOpcao.Calculadora:
+                    //Calcular dois valores
+                    Exercicio3.Calculadora();
+                    break;
+                case MenuOpcao.ContarCaracteres:
+                    //Crie um programa em que o usuário digita uma ou mais palavras e é exibido a quantidade de caracteres que a palavra inserida tem.
+                    Exercicio4.Palavra();
+                    break;
+                case MenuOpcao.ValidarPlaca:
+                    //Crie um programa em que o usuário precisa digitar a placa de um veículo e o programa verifica se a placa é válida, seguindo o padrão brasileiro válido até 2018
+                    Exercicio5.PlacaVeiculo();
+                    break;
+                case MenuOpcao.DataAtual:
+                    // Crie um programa que solicita ao usuário a exibição da data atual em diferentes formatos
+                    Exercicio6.DataAtual();
+                    break;
+                case MenuOpcao.NumeroPrimo:
+                    // Crie um programa que verifique se um número é primo.
+                    Exercicio7.numeroPrimo();
+                    break;
+                case MenuOpcao.SequenciaFibonacci:
+                    Fibonacci.numerosFibonacci();
+                    break;
+                case MenuOpcao.Inverternumeros:
+                    InverterNumero.inverte();
+                    break;
+                case MenuOpcao.VerificarVogais:
+                    Vogais.veificaVogais();
+                    break;
+                case MenuOpcao.VerificarNotas:
+                    Caixa.caixaEletronico();
+                    break;
+                case MenuOpcao.VerificarCpf:
+                    VerificarCpf.cpf();
+                    break;
+                default:
+                    Console.WriteLine("Opção invalida");
+                    break;
+            }
+        }
+    }
+    static void exibirMenu()
+    {
         Console.WriteLine("Escolha o exercício:\n" +
+                  "0 - Sair do programa \n" +
                   "1 - Nome do usuário\n" +
                   "2 - Concatenar nome completo\n" +
                   "3 - Calculadora\n" +
@@ -20,67 +87,12 @@ class Program
                   "10 - Verificar quantidade de vogais\n" +
                   "11 - Caixa eletronico\n" +
                   "12 - Validar Cpf");
-
-        int opcao = int.Parse(Console.ReadLine());
-
-        MenuOpcao escolha = (MenuOpcao)opcao;
-        
-        switch (escolha)
-        {
-            case MenuOpcao.NomeUsuario:
-                //Crie um programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas personalizada com o nome dele é exibida:  Olá, Welisson! Seja muito bem-vindo!
-                Exercicio1.NomeUsuario();
-                break;
-            case MenuOpcao.NomeCompleto:
-                //Crie um programa que concatene um nome e um sobrenome inseridos pelo usuário e ao final exiba o nome completo.
-                Exercicio2.NomeSobrenome();
-                break;
-            case MenuOpcao.Calculadora:
-                //Calcular dois valores
-                Exercicio3.Calculadora();
-                break;
-            case MenuOpcao.ContarCaracteres:
-                //Crie um programa em que o usuário digita uma ou mais palavras e é exibido a quantidade de caracteres que a palavra inserida tem.
-                Exercicio4.Palavra();
-                break;
-            case MenuOpcao.ValidarPlaca:
-                //Crie um programa em que o usuário precisa digitar a placa de um veículo e o programa verifica se a placa é válida, seguindo o padrão brasileiro válido até 2018
-                Exercicio5.PlacaVeiculo();
-                break;
-            case MenuOpcao.DataAtual:
-                // Crie um programa que solicita ao usuário a exibição da data atual em diferentes formatos
-                Exercicio6.DataAtual();
-                break;
-            case MenuOpcao.NumeroPrimo:
-                // Crie um programa que verifique se um número é primo.
-                Exercicio7.numeroPrimo();
-                break;
-            case MenuOpcao.SequenciaFibonacci:
-                Fibonacci.numerosFibonacci();
-                break;
-            case MenuOpcao.Inverternumeros:
-                InverterNumero.inverte();
-                break;
-            case MenuOpcao.VerificarVogais:
-                Vogais.veificaVogais();
-                break;
-            case MenuOpcao.VerificarNotas:
-                Caixa.caixaEletronico();
-                break;
-            case MenuOpcao.VerificarCpf:
-                VerificarCpf.cpf();
-                break;
-            default:
-                Console.WriteLine("Opção invalida");
-                break;
-        }
-
-
     }
 }
 
 public enum MenuOpcao
 {
+    Sair = 0,
     NomeUsuario = 1,
     NomeCompleto = 2,
     Calculadora = 3,
